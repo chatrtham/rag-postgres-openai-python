20d8fe30d4373e9290dbd18c012f6610393fd5e7
Chatrtham Chatramornrat <peenpeen21202@gmail.com>
2024-07-26 05:00:41 +0000

    Remove static files from .gitignore

---
+++ src/static/assets/NoPage-0376421b.js
@@ -0,0 +1,2 @@
+import{bp as o}from"./vendor-9c5496e3.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-0376421b.js.map
